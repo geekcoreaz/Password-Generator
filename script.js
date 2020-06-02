f532841d59eafca597ddd162d366547795cc9c5d
Wesley Stroeber <wesley@geekcore.org>
2020-06-01 18:02:57 -0700

    Updated JS to not run automatically and output to password box

--- script.js
+++ script.js
@@ -6,47 +6,45 @@ let symbols = "!@#$%^&*()_+-=";
 let userPass = "";
 let passGrp = "";
 
+// Prompt to start Password Generation
+window.onload = alert("Please click 'Generate password' to create a random password.");
+
 // Start Password Generation
 document.getElementById("generate").addEventListener("click", generatePassword);
 
-// Call function to generate password 
-generatePassword();
-
-
 function generatePassword() {
   // Set Password length
-  let passLength = prompt("Please enter your password length between 8 and 128 numbers");
+  let passLength = prompt("Please enter your password length between 8 and 128 numbers.");
 
   if (passLength < 8 || passLength > 128) {
-    alert("Please enter your password length between 8 and 128 numbers!");
-  }
+    alert("Please start over and enter your password length between 8 and 128 numbers!");
+  } else {
+    // Set Password options
+    let selectAbc = confirm("Would you like to use lower case letters?");
+    let selectAbcUpper = confirm("Would you like to use upper case letters?");
+    let selectNum = confirm("Would you like to use numbers?");
+    let selectSymbols = confirm("Would you like to use symbols?");
 
-  // Set Password options
-  let selectAbc = confirm("Would you like to use lower case letters?");
-  let selectAbcUpper = confirm("Would you like to use upper case letters?");
-  let selectNum = confirm("Would you like to use numbers?");
-  let selectSymbols = confirm("Would you like to use symbols?");
 
+    //Generate password
 
-  //Generate password
-
-  if (selectAbc) {
-    passGrp += abc;
-  }
-  if (selectAbcUpper) {
-    passGrp += abcUpper;
-  }
-  if (selectNum) {
-    passGrp += num;
-  }
-  if (selectSymbols) {
-    passGrp += symbols;
-  }
-  for (let i = 0; i < passLength; i++) {
-    userPass += passGrp.charAt(
-      Math.floor(Math.random() * passGrp.length)
-    );
+    if (selectAbc) {
+      passGrp += abc;
+    }
+    if (selectAbcUpper) {
+      passGrp += abcUpper;
+    }
+    if (selectNum) {
+      passGrp += num;
+    }
+    if (selectSymbols) {
+      passGrp += symbols;
+    }
+    for (let i = 0; i < passLength; i++) {
+      userPass += passGrp.charAt(Math.floor(Math.random() * passGrp.length));
+    }
+    // Put password on page
+    document.getElementById("password").innerHTML = userPass;
   }
 }
-// Write generated password on page
-document.getElementById("password").innerHTML = userPass;
+
